0d48a05d0317a7d5930221442c795a7817fd08dd
Izzy Moriguchi <izumimoriguchi@gmail.com>
2019-06-11 10:33:25 -0700

    Add spec and test for close frame exchange

--- src/test/java/org/reaktivity/specification/amqp/streams/ConnectionIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/ConnectionIT.java
@@ -60,4 +60,16 @@ public class ConnectionIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/close.exchange/client",
+        "${scripts}/close.exchange/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldExchangeClose() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
