824208cd6642d48e5b9fed70ec0589a8f2ebceae
Izzy Moriguchi <izumimoriguchi@gmail.com>
2019-06-12 12:12:25 -0700

    Add test method for channel max exceeded case

--- src/test/java/org/reaktivity/specification/amqp/streams/SessionIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/SessionIT.java
@@ -60,4 +60,16 @@ public class SessionIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/begin.channel.max.exceeded/client",
+        "${scripts}/begin.channel.max.exceeded/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldBeErrorWhenChannelMaxExceeded() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
