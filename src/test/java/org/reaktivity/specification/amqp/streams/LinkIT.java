65be95cad65bc34d963c56a2df706be651a79017
Izzy Moriguchi <izumimoriguchi@gmail.com>
2019-06-11 17:00:07 -0700

    Add spec and test for transfer as sender case

--- src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
@@ -84,4 +84,16 @@ public class LinkIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/transfer.as.sender/client",
+        "${scripts}/transfer.as.sender/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldSendTransferAndReceiveDisposition() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
