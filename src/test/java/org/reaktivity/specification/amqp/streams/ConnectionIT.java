bff9288d2f9b3c519fa7d2bc6990fe1d64192570
Izzy Moriguchi <izumimoriguchi@gmail.com>
2020-09-22 14:04:47 -0700

    Update close.timeout scripts

--- src/test/java/org/reaktivity/specification/amqp/streams/ConnectionIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/ConnectionIT.java
@@ -145,4 +145,17 @@ public class ConnectionIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/close.timeout/client",
+        "${scripts}/close.timeout/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldCloseConnectionWhenCloseTimeoutExpired() throws Exception
+
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
