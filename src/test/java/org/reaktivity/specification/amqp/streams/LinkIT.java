78b9f95fe5298f5033e48f9941f914cee7d9760d
Izzy Moriguchi <izumimoriguchi@gmail.com>
2020-10-19 12:28:52 -0700

    Add flow.without.handle scenario

--- src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
@@ -1392,4 +1392,16 @@ public class LinkIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/flow.without.handle/client",
+        "${scripts}/flow.without.handle/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldSupportFlowWithoutHandle() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
