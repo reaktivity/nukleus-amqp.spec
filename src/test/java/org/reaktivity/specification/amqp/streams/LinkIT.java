ad6290343ebd964998494bc4334e745b82fdd7e5
Izzy Moriguchi <izumimoriguchi@gmail.com>
2020-10-19 14:26:33 -0700

    Add flow.with.unattached.handle scenario

--- src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
@@ -1392,4 +1392,16 @@ public class LinkIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/flow.with.unattached.handle/client",
+        "${scripts}/flow.with.unattached.handle/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldRejectFlowWithUnattachedHandle() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
