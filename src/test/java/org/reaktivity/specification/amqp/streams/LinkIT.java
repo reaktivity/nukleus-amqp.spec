cf59edd47056a620acbf65be1981b3b43021a1e5
Izzy Moriguchi <izumimoriguchi@gmail.com>
2020-10-19 17:22:01 -0700

    Add reject.flow.with.inconsistent.fields scenarios

--- src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
+++ src/test/java/org/reaktivity/specification/amqp/streams/LinkIT.java
@@ -1392,4 +1392,16 @@ public class LinkIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+        "${scripts}/reject.flow.with.inconsistent.fields/client",
+        "${scripts}/reject.flow.with.inconsistent.fields/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/amqp#0\"")
+    public void shouldCloseConnectionWhenFlowWithInconsistentFields() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
